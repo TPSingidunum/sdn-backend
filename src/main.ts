d5b1ea796821770da117b9a48c2bf1b9543226d4
TeodorPetrovic <teodor.z.petrovic@gmail.com>
2024-11-15 08:23:04 +0100

    Added allow all cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for all origins
+  app.enableCors({
+    origin: '*', // Allows requests from any origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // HTTP methods allowed
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Allowed headers
+  });
+
   await app.listen(3000);
 }
 bootstrap();
